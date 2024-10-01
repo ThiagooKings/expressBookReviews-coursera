b7882a04dff4eda2620366e736ad3083cc506708
Thiago Reis <thiagomreeis@gmail.com>
2024-09-30 23:39:52 -0300

    terminando todos os exercicios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,91 +4,113 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+//Register new user
 public_users.post("/register", (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let firstName = req.body.firstName;
-  let lastName = req.body.lastName;
-  let email = req.body.email;
+  let { username, password, firstName, lastName, email } = req.body;
 
-  if (!username || !password) {
-    return res
-      .status(404)
-      .json({ message: "User name and/or passoword are empty!" });
-  }
-
-  if (!isValid(username)) {
-    return res.status(404).json({ message: "User already exists!" });
-  }
-
-  let newUser = { firstName, lastName, username, email, password };
-  users.push(newUser);
-
-  return res.status(201).send(JSON.stringify({ newUser }));
+  return new Promise((resolve, reject) => {
+    if (!username || !password) {
+      reject(
+        res
+          .status(404)
+          .json({ message: "User name and/or password are empty!" })
+      );
+    } else if (!isValid(username)) {
+      reject(res.status(404).json({ message: "User already exists!" }));
+    } else {
+      let newUser = { firstName, lastName, username, email, password };
+      users.push(newUser);
+      resolve(res.status(201).send(JSON.stringify({ newUser })));
+    }
+  }).catch((error) => {
+    res.status(500).json({ message: "Error registering user", error: error });
+  });
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.send(JSON.stringify({ books }));
+public_users.get("/", (req, res) => {
+  return new Promise((resolve) => {
+    resolve(res.send(JSON.stringify({ books })));
+  }).catch((error) => {
+    res.status(500).json({ message: "Error fetching book list.", error });
+  });
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", (req, res) => {
   let isbn = req.params.isbn;
 
-  if (books[isbn]) {
-    return res.send(JSON.stringify(books[isbn]));
-  }
-
-  return res.status(404).json({ message: "Book not found!" });
+  return new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(res.send(JSON.stringify(books[isbn])));
+    } else {
+      reject(res.status(404).json({ message: "Book not found!" }));
+    }
+  }).catch((error) => {
+    res.status(500).json({ message: "Error fetching book by ISBN.", error });
+  });
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", (req, res) => {
   let author = req.params.author;
-  let foundByAuthor = [];
 
-  for (let i in books) {
-    if (books[i].author === author) {
-      foundByAuthor.push(books[i]);
+  return new Promise((resolve, reject) => {
+    let foundByAuthor = [];
+
+    for (let i in books) {
+      if (books[i].author === author) {
+        foundByAuthor.push(books[i]);
+      }
     }
-  }
 
-  if (foundByAuthor) {
-    return res.send(JSON.stringify({ foundByAuthor }));
-  }
-
-  return res.status(404).json({ message: "Book not found!" });
+    if (foundByAuthor.length > 0) {
+      resolve(res.send(JSON.stringify({ foundByAuthor })));
+    } else {
+      reject(res.status(404).json({ message: "Book not found!" }));
+    }
+  }).catch((error) => {
+    res.status(500).json({ message: "Error fetching book by author.", error });
+  });
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", (req, res) => {
   let title = req.params.title;
-  let foundByTitle;
 
-  for (let i in books) {
-    if (books[i].title === title) {
-      foundByTitle = books[i];
-      break;
+  return new Promise((resolve, reject) => {
+    let foundByTitle = null;
+
+    for (let i in books) {
+      if (books[i].title === title) {
+        foundByTitle = books[i];
+        break;
+      }
     }
-  }
 
-  if (foundByTitle) {
-    return res.send(JSON.stringify({ foundByTitle }));
-  }
-
-  return res.status(404).json({ message: "Book not found!" });
+    if (foundByTitle) {
+      resolve(res.send(JSON.stringify({ foundByTitle })));
+    } else {
+      reject(res.status(404).json({ message: "Book not found!" }));
+    }
+  }).catch((error) => {
+    res.status(500).json({ message: "Error fetching book by title.", error });
+  });
 });
 
-//  Get book review
-public_users.get("/review/:isbn", function (req, res) {
+//  Get book review by isbn
+public_users.get("/review/:isbn", (req, res) => {
   let isbn = req.params.isbn;
 
-  if (books[isbn]) {
-    return res.send(JSON.stringify(books[isbn].reviews));
-  }
-
-  return res.status(404).json({ message: "Book not found!" });
+  return new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(res.send(JSON.stringify(books[isbn].reviews)));
+    } else {
+      reject(res.status(404).json({ message: "Book not found!" }));
+    }
+  }).catch((error) => {
+    res.status(500).json({ message: "Error fetching book review.", error });
+  });
 });
 
 module.exports.general = public_users;
